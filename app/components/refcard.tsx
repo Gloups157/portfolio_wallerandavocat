448e9c1ca4ecd4041ebd29bf1cde0d3c28588ab9
Gloups157 <wallerandavocat@sfr.fr>
2025-01-23 21:05:55 +0100

    refcards changes

--- app/components/refcard.tsx
+++ app/components/refcard.tsx
@@ -7,30 +7,42 @@ interface RefcardProps {
     url: string;
     cover: string;
     languages: string[];
-    tags: string[];
+    isExpanded: boolean;
+    onClick: () => void;
 }
 
-const Refcard: React.FC<RefcardProps> = ({title, description, url, cover, languages, tags}) => {
+const Refcard: React.FC<RefcardProps> = ({title, description, url, cover, languages, isExpanded, onClick}) => {
     return (
-        <div>
-            <Image
-                src={cover}
-                alt={cover}
+        <div className={`${isExpanded ? 'w-1/2 h-lvh z-10 absolute left-1/4 right-1/4' : 'flex relative w-72 h-96'} transition-all duration-700`}
+             onClick={onClick}>
+            <Image className="rounded-3xl inset-0 z-0"
+                   src={cover}
+                   alt={cover}
+                   layout="fill"
+                   objectFit="cover"
             />
-            <h3>
-                {title}
-            </h3>
-            <div>
-                {languages.map((language, index) => (
-                    <Image key={index} src={language} alt="language" width={50} height={50}/>
-                ))}
+            <div className="flex flex-col justify-end inset-10 p-10">
+                <div className="relative flex justify-end text-right">
+                    <h3>{title}</h3>
+                    <div className="">
+                        {languages.map((language, index) => (
+                            <Image
+                                key={index}
+                                src={language}
+                                alt={language}
+                                width={50}
+                                height={50}
+                            />
+                        ))}
+                    </div>
+                </div>
+                {isExpanded && (
+                    <>
+                        <p>{description}</p>
+                        <a href={url}>Lien du projet</a>
+                    </>
+                )}
             </div>
-            <p>
-                {description}
-            </p>
-            <a href={url}>
-               Lien du projet
-            </a>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+'use client';
 import Refcard from "@/app/components/refcard";
 import RefcardData from "@/app/data/refcards.json";
+import {useState} from "react";
 
 interface RefcardData {
     title: string;
@@ -11,18 +13,75 @@ interface RefcardData {
 }
 
 export default function Home() {
+    const [expandedCardIndex, setExpandedCardIndex] = useState<number | null>(null);
   return (
-      <main className="">
-        <h1>
-            Wallerand Avocat
-        </h1>
+    <main className="flex flex-col bg-repeat" style={{backgroundImage: "url('/images/paper_bg.png')"}}>
 
-          <div>
-              {RefcardData.map() => (
-                    <Refcard  />
-                )}
-              }
+        <div className="flex justify-around mt-72 -top-16">
+            <div className="border-4 w-60 h-96 ml-72">
+            </div>
+            <div className="absolute top-5">
+                <img src="/images/dotted.png" alt="pointillés"/>
+                <img className="mt-32" src="/images/dotted-1.png" alt="pointillés"/>
+            </div>
+            <h1 className="flex justify-center self-center text-5xl">
+                Wallerand Avocat
+            </h1>
+            <div className="border-4 w-60 h-96 mr-72">
+            </div>
+        </div>
+
+        <div className="flex align-middle p-10 mt-96">
+              <h2 className="">Profil</h2>
+              <div className="flex flex-col self-center text-3xl">
+                  <p>
+                      Salut, je m'appelle Wallerand et suis un passioné d'informatique et particulièrement de la
+                      Programmation Orientée Objet.
+                      voulant au départ exprimer ma créativité aussi bien dans le graphisme,
+                      la modélisation 3D ou encore la musique, j'ai tout naturellement voulu apprendre à faire des jeux
+                      vidéos avec le C# et Unity.
+                  </p>
+                  <p className="mt-10">
+                      Actuellement en deuxième année de BUT Informatique en section internationnale à l'université de
+                      Bordeaux,
+                      j'ai un stage de deux mois de prévu dans le domaine de l'aéronautique en avril et je recherche
+                      activement une alternance dans le développement d'application pour juillet 2025.
+                  </p>
+              </div>
+              <img className="w-1/2 mr-40" src="/images/covers/bench.jpg" alt="Portrait"/>
+          </div>
+
+          <div className="flex flex-wrap gap-2.5 mt-96">
+              {RefcardData.map((data: RefcardData, index: number) => (
+                  <Refcard key={index}
+                           title={data.title}
+                           description={data.description}
+                           url={data.url}
+                           cover={getCoverImage(data.cover)}
+                           languages={data.languages.map((language) => getLanguageIcon(language))}
+                           isExpanded={expandedCardIndex === index}
+                           onClick={() => setExpandedCardIndex(expandedCardIndex === index ? null : index)}
+                  />
+              ))}
           </div>
       </main>
   );
+
+    function getCoverImage(cover: string): string {
+        const filePath = "/images/covers/" + cover;
+        if (true) {
+            return filePath;
+        } else {
+            throw new Error(`File not found: ${filePath}`);
+        }
+  }
+
+  function  getLanguageIcon (language: string) : string {
+      const filePath = "/images/icons/languages/" + language + ".png";
+      if (true) {
+          return filePath;
+      } else {
+          throw new Error(`File not found: ${filePath}`);
+      }
+    }
 }
